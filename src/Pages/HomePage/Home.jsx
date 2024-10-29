42da7636758118a91e9b23d138d81a9e710c58ce
eduardooarruda <eduardodossantosarrudag@gmail.com>
2024-10-28 22:18:34 -0300

    criação componente Section

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import './Home.css'
 import Slide from "../../Components/Slide/Slide";
 import Colecao from "../../Components/Colecao/Colecao";
+import Section from "../../Components/Section/Section";
+
 const Home = () => {
   return (
    <>
-   <Slide/>
-   <Colecao/>
+      <Slide />
+      <Section />
+      <Colecao />
    </>
   );
 };
