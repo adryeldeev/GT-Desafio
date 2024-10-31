cb9a33e2c7b0b23461ffb4cdeea8526e2c60a383
adryel martins <adryeldev@hotmail.com>
2024-10-31 13:14:54 -0300

    atualizacao card2

--- src/Components/ProductDetails/ProducDetails.jsx
+++ src/Components/ProductDetails/ProducDetails.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
 import './ProductDetails.css';
-import IMGLayer from '../../assets/Layer.png'
+import IMGLayer from '../../assets/Layer.png';
+import Arrow from '../../assets/Line.png';
+
 const ProductDetails = () => {
   let cards = [
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
+    {
+      title: 'Tênis',
+      descricao: 'K-Swiss V8 - Masculino',
+      preco: 200,
+      desconto: 140,
+      imagem: IMGLayer,
+    },
     {
       title: 'Tênis',
       descricao: 'K-Swiss V8 - Masculino',
@@ -14,30 +65,35 @@ const ProductDetails = () => {
 
   return (
     <>
-    <div className="product-container">
+      <div className="product-container">
         <div className="product-header">
           <p>Produtos em alta</p>
           <div className="view-all">
-            <p>Ver todos</p>
+            <p>Ver todos
+               </p>
+            <img src={Arrow} alt="" />
           </div>
         </div>
         <div className="product-gallery">
           {cards.map((item, index) => (
-          <div className="product-card">
-          <div className="product-discount">
+            <div className="product-card" key={index}>
+             {index < 2 && 
+             (
+              <div className="product-discount">
               {((item.preco - item.desconto) / item.preco * 100).toFixed(0)}% OFF
-          </div>
-          <img src={item.imagem} alt={item.title} className="product-image" />
-          <p className="product-title">{item.title}</p>
-          <span className="product-description">{item.descricao}</span>
-          <div className="product-price">
-              <span className="original-price">${item.preco.toFixed(2)}</span>
-              <span className="discounted-price">${item.desconto.toFixed(2)}</span>
-          </div>
-      </div>
+            </div>
+             )}
+              <img src={item.imagem} alt={item.title} className="product-image" />
+              <p className="product-title">{item.title}</p>
+              <span className="product-description">{item.descricao}</span>
+              <div className="product-price">
+                <span className="original-price">${item.preco.toFixed(2)}</span>
+                <span className="discounted-price">${item.desconto.toFixed(2)}</span>
+              </div>
+            </div>
           ))}
         </div>
-    </div>
+      </div>
     </>
   );
 };
